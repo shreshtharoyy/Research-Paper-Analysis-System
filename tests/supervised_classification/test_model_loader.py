405b623035aff0eaaa72acb1fe4f6b727bd437be
shreshtharoyy <shreshtharoy16@gmail.com>
2026-06-08 17:20:45 +0530

    Add model loading validation test

---
+++ tests/supervised_classification/test_model_loader.py
@@ -0,0 +1,5 @@
+from pipeline.classification.supervised.model_loader import model
+from pipeline.classification.supervised.model_loader import tokenizer
+
+print(type(model))
+print(type(tokenizer))
