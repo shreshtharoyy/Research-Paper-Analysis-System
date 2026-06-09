3f1460794dcdf31e3278fc61812b2cb73e8a0bcc
shreshtharoyy <shreshtharoy16@gmail.com>
2026-06-09 17:29:12 +0530

    Update ModernBERT inference to use abstract extraction

--- tests/supervised_classification/test_classifier.py
+++ tests/supervised_classification/test_classifier.py
@@ -1,6 +1,6 @@
 from pipeline.classification.supervised.classifier import classify_paper
 from pipeline.pdf_processing import extract_text_from_pdf, clean_text
-from pipeline.abstract_extraction import extract_abstract
+from pipeline.section_extraction import extract_abstract
 
 text = extract_text_from_pdf("sample4.pdf")
 text = clean_text(text)
