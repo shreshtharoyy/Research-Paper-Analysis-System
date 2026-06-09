0fa0cb7f1ef0a61b2eb6bc0d8e12c74b80163703
shreshtharoyy <shreshtharoy16@gmail.com>
2026-06-09 21:28:31 +0530

    Add DistilBART model loader for abstractive summarization

---
+++ pipeline/summarization/model_loader.py
@@ -0,0 +1,5 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model_name = "sshleifer/distilbart-cnn-12-6"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
