53dafa895f0d4dc17e6fb1e73c24fd7231986e05
shreshtharoyy <shreshtharoy16@gmail.com>
2026-06-11 13:29:04 +0530

    Implement statistical candidate generation for keyword extraction

--- pipeline/keyword_extraction/candidate_generator.py
+++ pipeline/keyword_extraction/candidate_generator.py
@@ -1,28 +1,21 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
-def is_validate_candidate(candidate: str) -> bool:
-    words = candidate.split()
-
-    if len(words)<2:
-        return False
-    if any(char.isdigit() for char in candidate):
-        return False
-    
-    return True
-
 def generate_candidates(text:str) -> list[str]:
-    vectorizer = CountVectorizer(ngram_range=(2,3), stop_words="english", lowercase=True)
+    vectorizer = CountVectorizer(ngram_range=(2,3), stop_words="english", lowercase=True, token_pattern=r"(?u)\b[A-Za-z][A-Za-z-]+\b",
+        max_features=300)
 
     vectorizer.fit([text])
-    candidates = vectorizer.get_feature_names_out()
-    
+    candidates = vectorizer.get_feature_names_out().tolist()
+
     cleaned_candidates = []
 
     for candidate in candidates:
-        if is_validate_candidate(candidate):
-            cleaned_candidates.append(candidate)
 
-    return cleaned_candidates
+        if len(candidate.split()) > 3:
+            continue
+        if len(candidate) < 3:
+            continue
 
+        cleaned_candidates.append(candidate)
 
-
+    return cleaned_candidates 
