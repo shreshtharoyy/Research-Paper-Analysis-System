6a8c8e6754f501f0547c2abc365106612086de7b
shreshtharoyy <shreshtharoy16@gmail.com>
2026-06-14 16:39:32 +0530

    Update file path for testing mmr

--- tests/keyword_extraction/test_mmr.py
+++ tests/keyword_extraction/test_mmr.py
@@ -7,29 +7,13 @@ from pipeline.keyword_extraction.candidate_validator import is_valid_pos_pattern
 from pipeline.keyword_extraction.embedding import generate_embeddings
 from pipeline.keyword_extraction.mmr import maximal_marginal_relevance
 
+text = extract_text_from_pdf(
+    "sample_papers/online_shopping_behavior.pdf"
+)
 
-# text = extract_text_from_pdf(
-#     "sample_papers/online_shopping_behavior.pdf"
-# )
+text = clean_text(text)
 
-# text = clean_text(text)
-
-# abstract = extract_abstract(text)
-
-abstract = """We present YOLO, a new approach to object detection.
-Prior work on object detection repurposes classifiers to perform detection. Instead, we frame object detection as a regression problem to spatially separated bounding boxes and
-associated class probabilities. A single neural network predicts bounding boxes and class probabilities directly from
-full images in one evaluation. Since the whole detection
-pipeline is a single network, it can be optimized end-to-end
-directly on detection performance.
-Our unified architecture is extremely fast. Our base
-YOLO model processes images in real-time at 45 frames
-per second. A smaller version of the network, Fast YOLO,
-processes an astounding 155 frames per second while
-still achieving double the mAP of other real-time detectors. Compared to state-of-the-art detection systems, YOLO
-makes more localization errors but is less likely to predict
-false positives on background. Finally, YOLO learns very
-general representations of objects. It outperforms other detection methods, including DPM and R-CNN, when generalizing from natural images to other domains like artwork"""
+abstract = extract_abstract(text)
 
 candidates = generate_candidates(abstract)
 
