edfe32043b4f864dd9781d1973ed0083e96f6ede
shreshtharoyy <shreshtharoy16@gmail.com>
2026-06-16 10:15:30 +0530

    Allow frontendv1 dev origin in CORS

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ app.add_middleware(
 
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ],
 
     allow_credentials=True,
